0c30724f183c7628efe9dfc0db4af2e173f0db3e
dirceuovbelem <dirceu@fourtime.com>
2015-06-12 17:17:20 -0300

    Subindo alterações

--- app/src/main/java/dirceubelem/exemplo7/MainActivity.java
+++ app/src/main/java/dirceubelem/exemplo7/MainActivity.java
@@ -2,8 +2,7 @@ package dirceubelem.exemplo7;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 import dirceubelem.exemplo7.dao.DAOEstado;
 import dirceubelem.exemplo7.fw.DBHelper;
@@ -36,28 +35,7 @@ public class MainActivity extends ActionBarActivity {
         int q = d.quantidade();
         d.close();
 
+        Toast.makeText(this, "Quantidade de estados: " + q, Toast.LENGTH_LONG).show();
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
